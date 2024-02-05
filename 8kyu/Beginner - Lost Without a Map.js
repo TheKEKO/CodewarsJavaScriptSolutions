ceda2302a2ee251a95cbc987f553bb9b2377ccc5
Aleksandr F <a7214370@gmail.com>
2024-02-05 16:27:40 +0300

    Update Beginner - Lost Without a Map.js

--- 8kyu/Beginner - Lost Without a Map.js
+++ 8kyu/Beginner - Lost Without a Map.js
@@ -2,6 +2,25 @@
 
 // Solution 
 function maps(x) {
-let newArray = x.map(item => item * 2);
-  return newArray;
+  let newArray = x.map((item) => item * 2)
+  return newArray
+}
+
+// Solution 2
+function maps(x) {
+  return x.map((n) => n * 2)
+}
+
+// Solution 3
+maps = (x) => x.map((e) => e * 2)
+
+// Solution 4
+function maps(x) {
+  var newArr = []
+
+  x.map(function (y) {
+    newArr.push(y * 2)
+  })
+
+  return newArr
 }
