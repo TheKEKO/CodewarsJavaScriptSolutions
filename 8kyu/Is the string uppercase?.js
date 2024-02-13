11edff43be93c336f46d9e26b345e89f7aab2f56
TheKEKO <a7214370@gmail.com>
2024-02-13 10:51:11 +0300

    Solution 2

--- 8kyu/Is the string uppercase?.js
+++ 8kyu/Is the string uppercase?.js
@@ -4,3 +4,12 @@
 String.prototype.isUpperCase = function () {
   return !/[a-z]/.test(this)
 }
+
+// Solution 2
+String.prototype.isUpperCase = function () {
+  if (this.toString() == this.toUpperCase()) {
+    return true
+  } else {
+    return false
+  }
+}
