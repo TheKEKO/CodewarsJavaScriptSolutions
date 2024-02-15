cd3b78d059d89bb57e91c2af0e3bd1a69184316f
TheKEKO <a7214370@gmail.com>
2024-02-15 11:12:06 +0300

    Solution

--- 8kyu/Multiply the number.js
+++ 8kyu/Multiply the number.js
@@ -1,3 +1,13 @@
 // Multiply the number
 
-//
+//Solution
+function multiply(number) {
+  const absoluteValue = Math.abs(number)
+  const power = Math.floor(Math.log10(absoluteValue)) + 1
+
+  if (number < 0) {
+    return -number * -Math.pow(5, power)
+  } else {
+    return number * Math.pow(5, power)
+  }
+}
