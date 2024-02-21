c90270f20a84b4efcc136bdd8077c715ad7a22d0
TheKEKO <a7214370@gmail.com>
2024-02-21 11:30:10 +0300

    Solution

--- 8kyu/Pillars.js
+++ 8kyu/Pillars.js
@@ -1,1 +1,12 @@
 // Pillars
+
+// Solution
+function pillars(numPill, dist, width) {
+  if (width <= 10) {
+    return (numPill - 1) * dist * 100 + (numPill - 2) * 0
+  } else if (width <= 10 && dist >= 20) {
+    return (numPill - 1) * dist * 100 + (numPill - 2) * width
+  } else {
+    return (numPill - 1) * dist * 100 + (numPill - 2) * width
+  }
+}
