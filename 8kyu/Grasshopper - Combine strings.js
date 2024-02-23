333c1916fa2757f1eb14a52fa41085c4f90c4c43
TheKEKO <a7214370@gmail.com>
2024-02-23 21:50:45 +0300

    Solution 3

--- 8kyu/Grasshopper - Combine strings.js
+++ 8kyu/Grasshopper - Combine strings.js
@@ -6,4 +6,7 @@ function combineNames(firstName, lastName) {
 }
 
 // Solution 2
+combineNames = (firstName, lastName) => `${firstName} ${lastName}`
+
+// Solution 3
 const combineNames = (...names) => names.join(" ")
