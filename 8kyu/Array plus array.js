859e22e0836cb937669371c3ad240af5529b317a
TheKEKO <a7214370@gmail.com>
2024-02-28 18:14:22 +0300

    Solving algorithmic problem

--- 8kyu/Array plus array.js
+++ 8kyu/Array plus array.js
@@ -5,3 +5,5 @@ function arrayPlusArray(arr1, arr2) {
   return arr1.concat(arr2).reduce((a, b) => a + b)
 }
 
+// Solution 2
+arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((a, b) => a + b)
