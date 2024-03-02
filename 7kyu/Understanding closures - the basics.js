5fecb7cdc13ecb5437cd74a3622825bebb600913
TheKEKO <a7214370@gmail.com>
2024-03-02 13:23:11 +0300

    Implementing algorithm solution

--- 7kyu/Understanding closures - the basics.js
+++ 7kyu/Understanding closures - the basics.js
@@ -1,1 +1,13 @@
 // Understanding closures - the basics
+
+// Solution
+function buildFun(n) {
+  let res = []
+
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i
+    })
+  }
+  return res
+}
