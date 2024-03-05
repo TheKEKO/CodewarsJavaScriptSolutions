75c1b877605487f3691e3058e8f941a57ec888d5
TheKEKO <a7214370@gmail.com>
2024-03-05 15:47:04 +0300

    Implementing algorithm solution

--- 8kyu/All Star Code Challenge #18.js
+++ 8kyu/All Star Code Challenge #18.js
@@ -1,1 +1,14 @@
 // All Star Code Challenge #18
+
+// Solution
+function strCount(str, letter) {
+  let count = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++
+    }
+  }
+  return count
+}
+
+console.log(strCount("", "z"))
