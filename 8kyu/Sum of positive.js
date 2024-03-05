694da4abcae4eaddf5c97123d3a09d7dc1772a54
TheKEKO <a7214370@gmail.com>
2024-03-05 17:21:23 +0300

    Implementing algorithm solution

--- 8kyu/Sum of positive.js
+++ 8kyu/Sum of positive.js
@@ -4,3 +4,7 @@
 function positiveSum(arr) {
   return arr.filter((element) => element > 0).reduce((a, b) => a + b, 0)
 }
+
+// Solution 2
+const positiveSum = (arr) =>
+  arr.filter((element) => element > 0).reduce((a, b) => a + b, 0)
